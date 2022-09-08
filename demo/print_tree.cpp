777fc839ed2e780aa5e6358a0b15b26d00df462c
Luke Nihlen <luke.nihlen@gmail.com>
2022-09-08 12:47:48 -0400

    building, not parsing

---
+++ demo/print_tree.cpp
@@ -0,0 +1,33 @@
+// print_tree.cpp: prints the parse tree of an input .sc or .scd file
+#include "antlr4-runtime.h"
+#include "SCLexer.h"
+#include "SCParser.h"
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+      std::cerr << "usage: print_tree fileName.sc\n";
+      return -1;
+    }
+
+    antlr4::ANTLRFileStream input;
+    input.loadFromFile(std::string(argv[1]));
+
+    sprklr::SCLexer lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    tokens.fill();
+
+    sprklr::SCParser parser(&tokens);
+    antlr4::tree::ParseTree *tree = parser.root();
+
+    if (parser.getNumberOfSyntaxErrors()) {
+        std::cerr << "got " << parser.getNumberOfSyntaxErrors() << " syntax errors.\n";
+        return -1;
+    }
+
+    std::cout << tree->toStringTree(&parser) << std::endl;
+
+    return 0;
+}
